1bd682f6e5786c602675cfec5296d01e70f1ac9b
leggard <mrleggard@gmail.com>
2022-03-16 16:51:45 +0200

    made webcam video to stream to the browser (via fastapi)

---
+++ 0_the_project/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, StreamingResponse
+from starlette.templating import Jinja2Templates
+
+
+import cv2
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+camera = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+
+        ## read the camera frame
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+        yield (b'--frame\r\n' 
+               b'Content-Type: image/jpeg\r\n\r\n'
+               + frame +
+               b'\r\n')
+
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/video')
+def video():
+    return StreamingResponse(generate_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
