187ed62d31b311b8c07f9c4b4032904c33fa1902
leggard <mrleggard@gmail.com>
2022-03-24 22:33:16 +0200

    Some more minor fixes

--- 0_the_project/resources/resource_main.py
+++ 0_the_project/resources/resource_main.py
@@ -1,46 +1,14 @@
 from fastapi import APIRouter, WebSocket
 import asyncio
-import cv2
 
 from starlette.requests import Request
-from starlette.responses import HTMLResponse, StreamingResponse, PlainTextResponse
+from starlette.responses import HTMLResponse, PlainTextResponse
 from starlette.templating import Jinja2Templates
 
-# from services.service_get_webcam_stream import WebcamStream
+from services.service_get_webcam_stream import WebcamStream
 
 router_main = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
-# webcam_stream = WebcamStream()
-
-
-class WebcamStream():
-
-    def __init__(self):
-        self.webcam = None
-        self.webcam_resolution = None
-
-    def resize_webcam_resolution(self, frame, needed_height: int = 540):
-        resolution = frame.shape
-        coef = needed_height / resolution[0]
-        return int(resolution[1] * coef), int(resolution[0] * coef)
-
-    def start_stream(self):
-        self.webcam = cv2.VideoCapture(0)
-        _, frame = self.webcam.read()
-        self.webcam_resolution = self.resize_webcam_resolution(frame)
-        print(f'Webcam has turned on! With {self.webcam_resolution} resolution')
-
-    def generated_frame(self):
-        success, frame = self.webcam.read()
-        frame = cv2.resize(frame, self.webcam_resolution, interpolation=cv2.INTER_CUBIC)
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        return frame
-
-    def stop_stream(self):
-        self.webcam.release()
-        print('Webcam has turned off!')
 
 webcam_stream = WebcamStream()
 
@@ -51,10 +19,7 @@ def index(request: Request):
 
 @router_main.get('/video_start')
 def video_start():
-    try:
-        webcam_stream.start_stream()
-    except:
-        print('You tried to turn on the webcam and sucked')
+    webcam_stream.start_stream()
     return PlainTextResponse('You just turned on the camera')
 
 @router_main.websocket("/ws_video")
@@ -69,8 +34,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 @router_main.get('/video_stop')
 def video_stop():
-    try:
-        webcam_stream.stop_stream()
-    except:
-        print('You tried to turn off the webcam and sucked')
+    webcam_stream.stop_stream()
     return PlainTextResponse('You just turned off the camera')
--- 0_the_project/services/service_get_webcam_stream.py
+++ 0_the_project/services/service_get_webcam_stream.py
@@ -1,57 +1,40 @@
 import cv2
+import face_recognition
 
 
-class WebcamStream:
+class WebcamStream():
 
     def __init__(self):
         self.webcam = None
-
-    def get_webcam_stream(self):
-        return cv2.VideoCapture(0)
+        self.webcam_resolution = None
 
     def resize_webcam_resolution(self, frame, needed_height: int = 540):
         resolution = frame.shape
-        # print(type(frame)) numpy.ndarray
         coef = needed_height / resolution[0]
         return int(resolution[1] * coef), int(resolution[0] * coef)
 
-    def start_generating_frames(self):
+    def start_stream(self):
         self.webcam = cv2.VideoCapture(0)
-        print('services.service_get_webcam_stream.start_generating_frames(): The webcam stream was turned on')
-
-    def generated_frames(self):
-
-        while True:
-            success, frame = self.webcam.read()
-
-            if not success:
-                break
-            else:
-                # locations = face_recognition.face_locations(frame, model='small')
-                #
-                # for face_location in locations:
-                #     top_left = (face_location[3], face_location[0])
-                #     bottom_right = (face_location[1], face_location[2])
-                #     color = [0, 255, 0]
-                #     cv2.rectangle(frame, top_left, bottom_right, color, 3)
-
-                frame = cv2.resize(frame, self.resize_webcam_resolution(frame), interpolation=cv2.INTER_CUBIC)
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-
-            yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+        _, frame = self.webcam.read()
+        self.webcam_resolution = self.resize_webcam_resolution(frame)
+        print(f'Webcam has turned on! With {self.webcam_resolution} resolution')
 
     def generated_frame(self):
         success, frame = self.webcam.read()
 
-        if not success:
-            return None
-        else:
-            frame = cv2.resize(frame, self.resize_webcam_resolution(frame), interpolation=cv2.INTER_CUBIC)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            return b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+        # locations = face_recognition.face_locations(frame, model='small')
+        #
+        # for face_location in locations:
+        #     top_left = (face_location[3], face_location[0])
+        #     bottom_right = (face_location[1], face_location[2])
+        #     color = [0, 255, 0]
+        #     cv2.rectangle(frame, top_left, bottom_right, color, 3)
 
-    def stop_generating_frames(self):
+        frame = cv2.resize(frame, self.webcam_resolution, interpolation=cv2.INTER_CUBIC)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        return frame
+
+    def stop_stream(self):
         self.webcam.release()
-        print('services.service_get_webcam_stream.generate_frames(): The webcam was successfully released')
+        print('Webcam has turned off!')
