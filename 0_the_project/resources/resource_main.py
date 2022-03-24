68c5f1ae7d15d7d4e8bec6daca2a24fce9ce8972
leggard <mrleggard@gmail.com>
2022-03-24 22:18:06 +0200

    Freakin made this WebSocket work on the main page

--- 0_the_project/resources/resource_main.py
+++ 0_the_project/resources/resource_main.py
@@ -1,16 +1,48 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
+import asyncio
+import cv2
 
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, StreamingResponse, PlainTextResponse
 from starlette.templating import Jinja2Templates
 
-from services.service_get_webcam_stream import WebcamStream
+# from services.service_get_webcam_stream import WebcamStream
 
 router_main = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-webcam_stream = WebcamStream()
+# webcam_stream = WebcamStream()
 
+
+class WebcamStream():
+
+    def __init__(self):
+        self.webcam = None
+        self.webcam_resolution = None
+
+    def resize_webcam_resolution(self, frame, needed_height: int = 540):
+        resolution = frame.shape
+        coef = needed_height / resolution[0]
+        return int(resolution[1] * coef), int(resolution[0] * coef)
+
+    def start_stream(self):
+        self.webcam = cv2.VideoCapture(0)
+        _, frame = self.webcam.read()
+        self.webcam_resolution = self.resize_webcam_resolution(frame)
+        print(f'Webcam has turned on! With {self.webcam_resolution} resolution')
+
+    def generated_frame(self):
+        success, frame = self.webcam.read()
+        frame = cv2.resize(frame, self.webcam_resolution, interpolation=cv2.INTER_CUBIC)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        return frame
+
+    def stop_stream(self):
+        self.webcam.release()
+        print('Webcam has turned off!')
+
+webcam_stream = WebcamStream()
 
 @router_main.get('/', response_class=HTMLResponse)
 def index(request: Request):
@@ -20,25 +52,36 @@ def index(request: Request):
 @router_main.get('/video_start')
 def video_start():
     try:
-        webcam_stream.start_generating_frames()
+        webcam_stream.start_stream()
     except:
         print('You tried to turn on the webcam and sucked')
     return PlainTextResponse('You just turned on the camera')
 
 
-@router_main.get('/video')
-def video():
-    while True:
-        if not webcam_stream.webcam:
-            pass
-        else:
-            return StreamingResponse(webcam_stream.generated_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
+# @router_main.get('/video')
+# def video():
+#     while True:
+#         if not webcam_stream.webcam:
+#             pass
+#         else:
+#             return StreamingResponse(webcam_stream.generated_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
 
+
+@router_main.websocket("/ws_video")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        try:
+            await asyncio.sleep(0.04)
+            await websocket.send_bytes(webcam_stream.generated_frame())
+        except:
+            await asyncio.sleep(2)
+            print('Webcam is turned off')
 
 @router_main.get('/video_stop')
 def video_stop():
     try:
-        webcam_stream.stop_generating_frames()
+        webcam_stream.stop_stream()
     except:
         print('You tried to turn off the webcam and sucked')
     return PlainTextResponse('You just turned off the camera')
--- 0_the_project/static/main.js
+++ 0_the_project/static/main.js
@@ -10,22 +10,38 @@ button.addEventListener('click', () =>{
     }
 });
 
- function showHide(element_id, element_id_2) {
+function openSocket() {
+            websocket = new WebSocket("ws://127.0.0.1:8000/ws_video");
+            let msg = document.getElementById("video");
+            websocket.addEventListener('open', function (event) {
+                document.getElementById("status").innerHTML = "WebSocket connection is OPENED";
+            });
+            websocket.addEventListener('message', function (event) {
+                let context = msg.getContext("2d");
+                let image = new Image();
+                image.src = URL.createObjectURL(event.data);
+                image.addEventListener("load", function (event) {
+                    context.drawImage(image, 0, 0, msg.width, msg.height);
+                });
+            });
+        }
+
+function showHide(id_1, id_2) {
     var xmlHttp = new XMLHttpRequest();
-    if (document.getElementById(element_id, element_id_2)){
-        var obj = document.getElementById(element_id);
-        var obj2 = document.getElementById(element_id_2);
+
+    if (document.getElementById(id_1, id_2)){
+        var obj = document.getElementById(id_1);
+        var obj2 = document.getElementById(id_2);
+
         if (obj2.style.display == "none") {
             xmlHttp.open( "GET", "video_start", false ); // false for synchronous request
             xmlHttp.send( null );
             obj.style.display = "none"; // hide preview
-            obj2.style.display = "block"; // show video
-
+            obj2.style.display = "inline-block"; // show video
         }
         else {
             xmlHttp.open( "GET", "video_stop", false ); // false for synchronous request
             xmlHttp.send( null );
-//            document.location.reload();
             obj.style.display = "block"; // show preview
             obj2.style.display = 'none'; // hide video
         }
