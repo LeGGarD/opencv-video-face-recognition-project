8571fa1234224acd723fb04f724cdc1e79896133
zlata01 <zlata.kropta@gmail.com>
2022-03-27 18:04:33 +0300

    Made a form for adding users to the DB

--- the_project/resources/resource_add_user.py
+++ the_project/resources/resource_add_user.py
@@ -7,10 +7,10 @@ router_add_user = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-@router_add_user.get('/add_user/', response_class=HTMLResponse)
+@router_add_user.get('/add_user_step_1/', response_class=HTMLResponse)
 def get_add_user_page(request: Request):
-    return templates.TemplateResponse('add_user.html', {'request': request})
+    return templates.TemplateResponse('add_user_step_1.html', {'request': request})
 
-@router_add_user.post('/add_user/')
+@router_add_user.post('/add_user_step_1/')
 def recognize_face():
     pass
