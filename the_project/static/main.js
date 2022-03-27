17a34c6e0000ebe9a28252d6084e821ca9feb523
leggard <mrleggard@gmail.com>
2022-03-27 23:33:13 +0300

    Fixed start button. Proper websocket closing is coming

--- the_project/static/main.js
+++ the_project/static/main.js
@@ -1,59 +1,32 @@
-function openSocket() => {
+function openSocket() {
     let websocket = new WebSocket("ws://127.0.0.1:8000/ws_video");
     let msg = document.getElementById("video");
     websocket.onopen = (event) => {
         document.getElementById("status").innerHTML = "WebSocket connection is OPENED";
-    });
+    };
     websocket.onmessage = (event) => {
         let context = msg.getContext("2d");
         let image = new Image();
         image.src = URL.createObjectURL(event.data);
         image.onload = (event) => {
             context.drawImage(image, 0, 0, msg.width, msg.height);
-        });
-    });
+        };
+    };
 }
 
-function closeSocket() => {
+function closeSocket() {
     websocket.close();
 }
 
-//function showHide(id_1, id_2) {
-//    var xmlHttp = new XMLHttpRequest();
-//    var button = document.getElementById('start');
-//    if (document.getElementById(id_1, id_2)){
-//        var obj = document.getElementById(id_1);
-//        var obj2 = document.getElementById(id_2);
-//
-//        if (obj2.style.display == "none") {
-//            openSocket();
-//            xmlHttp.open( "GET", "/video_start", false ); // false for synchronous request
-//            xmlHttp.send( null );
-//            obj.style.display = "none"; // hide preview
-//            obj2.style.display = "inline-block"; // show video
-//            button.innerText = 'Stop';
-//            button.dataset.trigger = false;
-//        }
-//        else {
-//            xmlHttp.open( "GET", "/video_stop", false ); // false for synchronous request
-//            xmlHttp.send( null );
-//            closeSocket()
-//            obj.style.display = "block"; // show preview
-//            obj2.style.display = 'none'; // hide video
-//            button.innerText = 'Start';
-//            button.dataset.trigger = true;
-//        }
-//    }
-//    else alert("Элемент с id: " + element_id + " не найден!");
-//}
-
-function showHide(id_1, id_2) => {
+function showHide(id_1, id_2) {
     var xmlHttp = new XMLHttpRequest();
+    var button = document.getElementById('start');
     if (document.getElementById(id_1, id_2)){
         var obj = document.getElementById(id_1);
         var obj2 = document.getElementById(id_2);
 
         if (obj2.style.display == "none") {
+            openSocket();
             xmlHttp.open( "GET", "/video_start", false ); // false for synchronous request
             xmlHttp.send( null );
             obj.style.display = "none"; // hide preview
@@ -73,7 +46,8 @@ function showHide(id_1, id_2) => {
     else alert("Элемент с id: " + element_id + " не найден!");
 }
 
-function parsePhoto() => {
+
+function parsePhoto() {
     var xmlHttp = new XMLHttpRequest();
     var user_id = getElementById('user_id');
     var path = '/add_user_step_2/recognize_face/' + user_id;
